fb6db78346b85b9ec62180e67c6f6e3c564580ec
Aaron Shafovaloff <ashafovaloff@instructure.com>
2019-08-12 19:23:26 -0600

    Save volume nav scroll state

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import { useLocalStorageState } from "react-storage-hooks";
 import {
   SiteTitle,
   SiteTitleLink,
@@ -9,47 +10,57 @@ import {
   VolumeNumbersWrapper
 } from "./header.styles.js";
 
-export default class Header extends React.Component {
-  render() {
-    const {
-      volumeNumbers,
-      volumeNumber,
-      siteTitle,
-      siteDescription
-    } = this.props;
-    return (
-      <header>
-        <SiteTitleWrapper>
-          <SiteTitle>
-            <SiteTitleLink to="/">{siteTitle}</SiteTitleLink>
-          </SiteTitle>
-          <Subheading>{siteDescription}</Subheading>
-        </SiteTitleWrapper>
-        {volumeNumbers && (
-          <VolumeNumbersWrapper>
-            <VolumeNumbers>
-              {volumeNumbers.map(volumeNumber_ => (
-                <React.Fragment key={volumeNumber_}>
-                  {volumeNumber_ === 1 && (
-                    <>
-                      <span>Vol.</span>{" "}
-                    </>
-                  )}
-                  <VolumeNumber
-                    key={volumeNumber_}
-                    partiallyActive={volumeNumber != null}
-                    activeClassName={"active"}
-                    to={`/${volumeNumber_}/`}
-                    title={`Volume ${volumeNumber_}`}
-                  >
-                    {volumeNumber_}
-                  </VolumeNumber>
-                </React.Fragment>
-              ))}
-            </VolumeNumbers>
-          </VolumeNumbersWrapper>
-        )}
-      </header>
-    );
-  }
+export default function Header(props) {
+  const { volumeNumbers, volumeNumber, siteTitle, siteDescription } = props;
+
+  const [scrollLeft, setScrollState] = useLocalStorageState(
+    "volumeWrapperScrollLeft",
+    0
+  );
+
+  const wrapperEl = useRef(null);
+
+  useLayoutEffect(() => {
+    wrapperEl.current.scrollLeft = scrollLeft;
+  }, []);
+
+  const handleScroll = event => {
+    // console.debug(event.target.scrollLeft);
+    setScrollState(event.target.scrollLeft);
+  };
+
+  return (
+    <header>
+      <SiteTitleWrapper>
+        <SiteTitle>
+          <SiteTitleLink to="/">{siteTitle}</SiteTitleLink>
+        </SiteTitle>
+        <Subheading>{siteDescription}</Subheading>
+      </SiteTitleWrapper>
+      {volumeNumbers && (
+        <VolumeNumbersWrapper ref={wrapperEl} onScroll={handleScroll}>
+          <VolumeNumbers>
+            {volumeNumbers.map(volumeNumber_ => (
+              <React.Fragment key={volumeNumber_}>
+                {volumeNumber_ === 1 && (
+                  <>
+                    <span>Vol.</span>{" "}
+                  </>
+                )}
+                <VolumeNumber
+                  key={volumeNumber_}
+                  partiallyActive={volumeNumber != null}
+                  activeClassName={"active"}
+                  to={`/${volumeNumber_}/`}
+                  title={`Volume ${volumeNumber_}`}
+                >
+                  {volumeNumber_}
+                </VolumeNumber>
+              </React.Fragment>
+            ))}
+          </VolumeNumbers>
+        </VolumeNumbersWrapper>
+      )}
+    </header>
+  );
 }
